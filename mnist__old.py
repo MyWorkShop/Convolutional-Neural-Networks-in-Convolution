0f6e5718ccfa724aa2f6b7d71d518a840e7a7b9b
mwsht <ht@mws.site>
2018-06-18 14:13:02 +0800

    Change the logits layer out of the small cnn

---
+++ mnist__old.py
@@ -0,0 +1,143 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+# Imports
+import tensorflow as tf
+import time
+
+def small_cnn(x, phase_train):
+    # Convolutional Layer #1
+    conv1 = tf.layers.conv2d(
+        inputs=x,
+        filters=32,
+        kernel_size=[5, 5],
+        padding="same",
+        activation=tf.nn.relu)
+
+    # Convolutional Layer #2
+    conv2 = tf.layers.conv2d(
+        inputs=conv1,
+        filters=32,
+        kernel_size=[5, 5],
+        padding="same",
+        activation=tf.nn.relu)
+
+    # Pooling Layer #1
+    pool1 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
+
+    # Convolutional Layer #3 and Pooling Layer #2
+    conv3 = tf.layers.conv2d(
+        inputs=pool1,
+        filters=32,
+        kernel_size=[3, 3],
+        padding="same",
+        activation=tf.nn.relu)
+    conv4 = tf.layers.conv2d(
+        inputs=conv3,
+        filters=32,
+        kernel_size=[3, 3],
+        padding="same",
+        activation=tf.nn.relu)
+    pool2 = tf.layers.average_pooling2d(inputs=conv4, pool_size=[2, 2], strides=2)
+    pool2_dropout = tf.layers.dropout(inputs=pool2, rate=0.4, training=phase_train)
+
+    # Dense Layer
+    pool2_flat = tf.reshape(pool2_dropout, [-1, 4 * 4 * 32])
+    dense = tf.layers.dense(inputs=pool2_flat, units=256, activation=tf.nn.relu)
+    dropout = tf.layers.dropout(inputs=dense, rate=0.4, training=phase_train)
+
+    return dropout
+
+def cnnic(x):
+    phase_train = tf.placeholder(tf.bool)
+    m = 5
+    n = 5
+    stride = 3
+
+    x = tf.reshape(x, [-1, 28, 28, 1])
+
+    #CNNIC Layer
+    slicing = tf.TensorArray('float32', m * n)
+    for j in range(m):
+        for k in range(n):
+            slicing = slicing.write(
+                j * n + k, tf.slice(x, [0, j * stride, k * stride, 0],
+                            [-1, 16, 16, 1]))
+    scn_input = tf.reshape(slicing.concat(), [-1, 16, 16, 1])
+    slicing.close().mark_used()
+    
+    scn_output = tf.reshape(small_cnn(scn_input, phase_train), [m * n, -1 , 256])
+    scn_output = tf.reshape(tf.transpose(scn_output, [1, 0, 2]), [-1, m, n, 256])
+
+    pool = tf.layers.average_pooling2d(inputs=scn_output, pool_size=[2, 2], strides=1)
+    pool_flat = tf.reshape(pool, [-1, (m - 1) * (n - 1) * 256])
+
+    # Logits Layer
+    logits = tf.layers.dense(inputs=pool_flat, units=10)
+    return logits, phase_train
+
+
+
+
+def main(unused_argv):
+    mnist = tf.contrib.learn.datasets.load_dataset("mnist")
+
+    input_data = tf.placeholder(tf.float32, [None, 784])
+    output_data = tf.placeholder(tf.int64, [None])
+    y_model, phase_train= cnnic(input_data)
+
+    #Loss
+    cross_entropy = tf.losses.sparse_softmax_cross_entropy(
+            labels=output_data, logits=y_model)
+    cross_entropy = tf.reduce_mean(cross_entropy)
+
+    #Optimizer
+    rate = tf.placeholder(tf.float32)
+    train_step = tf.train.AdamOptimizer(rate).minimize(cross_entropy)
+
+    #Accuracy
+    correct_prediction = tf.equal(tf.argmax(y_model, 1), output_data)
+    correct_prediction = tf.cast(correct_prediction, tf.float32)
+
+    accuracy = tf.reduce_mean(correct_prediction)
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        t0 = time.clock()
+        rt = 1e-3
+        for i in range(60001):
+            # Get the data of next batch
+            batch = mnist.train.next_batch(100)
+            if (i % 600 == 0) and (i != 0):
+                if i == 30000:
+                    rt = 3e-4
+                if i == 42000:
+                    rt = 1e-4
+                if i == 48000:
+                    rt = 3e-5
+                if i == 54000:
+                    rt = 1e-5
+                # Print the accuracy
+                test_accuracy = 0
+                test_accuracy_once = 0
+                for index in range(200):
+                    accuracy_batch = mnist.test.next_batch(50)
+                    test_accuracy_once = sess.run(accuracy, feed_dict={
+                        input_data: accuracy_batch[0], output_data: accuracy_batch[1],
+                        phase_train: False})
+                    test_accuracy += test_accuracy_once
+                    test_accuracy_once = 0
+                print('%g, %g, %g' %
+                      (i / 600, test_accuracy / 200, (time.clock() - t0)))
+                t0 = time.clock()
+            # Train
+            _ = sess.run(
+                train_step,
+                feed_dict={input_data: batch[0],
+                           output_data: batch[1],
+                           phase_train: True,
+                           rate: rt})
+
+if __name__ == "__main__":
+    tf.app.run()
